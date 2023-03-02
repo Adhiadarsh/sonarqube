b5cb77e609f66ad193de2158f710516867349587
Adhiadarsh <113585910+Adhiadarsh@users.noreply.github.com>
2023-03-02 15:10:34 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -11,7 +11,7 @@
 public class HelloWorld {
 
     public void sayHello() {
-        System.out.println("Hello  I'm Adhi");
+        System.out.println("Hi friends");
     }
 
     public void notCovered() {
