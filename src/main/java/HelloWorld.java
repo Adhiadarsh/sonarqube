85696bfb5691a111ac95126d13447d19e8236c2d
Adhiadarsh <113585910+Adhiadarsh@users.noreply.github.com>
2023-03-02 15:51:59 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,14 +9,8 @@
 
 
 public class HelloWorld {
-
-    public void sayHello() {
-        System.out.println("Hi friends..");
+    public static void main(String[] args)
+    {
+        System.out.print("hello i'm Adhi");
     }
-
-    public void notCovered() {
-        System.out.println("This method is not covered by unit tests");
-    }
-
-
 }
